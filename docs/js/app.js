3f6fc60df43f5f8f352b8efe2bd168d766a49a88
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-07-29 23:13:47 -0400

    Done

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,8 +2,6 @@ const divContainer = document.querySelector(".container");
 const accessToken = "10221023993695824";
 const searchNameButton = document.getElementById("submit");
 
-
-
 searchNameButton.addEventListener("click", function(){
 const heroInfo = document.getElementById("heroInfo");
 const heroSection = document.getElementById("heroSection");
@@ -21,20 +19,23 @@ let item = document.createElement("li");
 item.id = "placeholderInfo";
 list.appendChild(item);
 
-
-
 for(let i =0; i< results.length; i++){
     let item = document.createElement("li");
     item.id = "placeholderInfo";
     item.innerHTML = `
+    <div class="heroNameContainer">
     <h3 id="details">${results[i].name}</H3>
-    <img src='${results[i].image.url}' />
-    <p id="details">${results[i].biography["full-name"]}</p>
-    <p id="details">${results[i].appearance.gender}</p>
-    <p id="details">${results[i].appearance.race}</p>
-    <p id="details">${results[i].biography.alignment}</p>
-    <p id="details">${results[i].biography.publisher}</p>
+    </div>
+    <img id="heroImage" src='${results[i].image.url}' />
+    <div class="heroDetailsContainer">
+    <p id="details"><label>Real Name: </label>${results[i].biography["full-name"]}</p>
+    <p id="details"><label>Gender: </label>${results[i].appearance.gender}</p>
+    <p id="details"><label>Race: </label>${results[i].appearance.race}</p>
+    <p id="details"><label>Good or bad: </label>${results[i].biography.alignment}</p>
+    <p id="details"><label>Publisher: </label>${results[i].biography.publisher}</p>
+    </div>
     
+    <div class="heroStatsContainer">
     <ul>
         <h4 id="details">Power Stats</h4>
         <li id="details"><label>Combat: </label>${results[i].powerstats.combat}</li>
@@ -44,6 +45,7 @@ for(let i =0; i< results.length; i++){
         <li id="details"><label>Speed: </label>${results[i].powerstats.speed}</li>
         <li id="details"><label>Strength: </label>${results[i].powerstats.strength}</li>
     </ul>
+    </div>
     `;
 
     list.appendChild(item);
@@ -52,6 +54,10 @@ for(let i =0; i< results.length; i++){
 
 heroSection.appendChild(list);
 
+
+
 console.log(data)
 });
 });
+
+
